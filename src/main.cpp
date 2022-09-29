db9ef3e48c9a81e472071f82f4e092c07f735a37
Thgear27 <vegasfernando2003@gmail.com>
2022-09-28 22:48:52 -0500

    Transfomation test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "model.hpp"
 #include "my_gl.hpp"
 #include "tgaimage.h"
+#include "matrix.hpp"
 #include <fstream>
 #include <iostream>
 
-constexpr int height { 1000 };
-constexpr int width { 1000 };
+constexpr int height { 500 };
+constexpr int width  { 500 };
 
 const char* african_head = "obj/african_head/african_head.obj";
 const char* african_head_texture_img = "obj/african_head/african_head_diffuse.tga";
@@ -20,30 +21,24 @@ const char* diablo3pose = "obj/diablo3_pose/diablo3_pose.obj";
 const char* diablo3pose_texture_img = "obj/diablo3_pose/diablo3_pose_diffuse.tga";
 
 int main() {
-    Model model1 { african_head, Model::Format::with_vt };
-
-    TGAImage img(width, height, TGAImage::Format::RGB);
-    TGAImage textureImg;
-
+    TGAImage img { width, height, TGAImage::Format::RGB };
+    Model modelo(african_head, Model::Format::with_vt);
+    TGAImage textureImg {};
     textureImg.read_tga_file(african_head_texture_img);
     textureImg.flip_vertically();
 
     float* zbuffer = new float[width * height];
-
     for (int i = 0; i < width * height; i++) {
         zbuffer[i] = -std::numeric_limits<float>::max();
     }
 
-
-    std::cout << "Renderizando...\n";
-    my_gl::simpleRender(model1, textureImg, zbuffer, img, vec3f { 0, 0, 1 });
+    my_gl::simpleRender(modelo, textureImg, zbuffer, img, vec3f { 0, 0, 1 });
     std::cout << "Renderizacion hecha...\n";
 
     img.flip_vertically();
-    img.write_tga_file("render6.tga");
-
-    delete[] zbuffer;
-    std::cout << "Hecho" << '\n';
+    img.write_tga_file("test0.tga");
+    std::cout << "done..." << '\n';
+    return 0;
 }
 
 // TODO:
