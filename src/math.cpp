b90f906128e419c6e506e9dee3ef2bccd1cb9d37
Thgear27 <vegasfernando2003@gmail.com>
2022-10-05 23:27:35 -0500

    inline added for performance

--- src/math.cpp
+++ src/math.cpp
@@ -1,40 +1,6 @@
 #include "math.hpp"
 #include <cmath>
 
-vec3f toBarycentricCoord(vec3f* verts, vec2f point) {
-    // Asumiendo r1, r2, r3 los vertices del triangulo, y p el punto
-    // Variables que representan vectores matematicos
-    vec3f r1_r2 = verts[1] - verts[0];
-    vec3f r1_r3 = verts[2] - verts[0];
-    vec3f p_r1 = verts[0] - (vec3f)point;
-
-    // vec3f x_values { r1_r2.x, r1_r3.x, p_r1.x };
-    // vec3f y_values { r1_r2.y, r1_r3.y, p_r1.y };
-    vec3f vecRes = crossProduct(vec3f { r1_r2.x, r1_r3.x, p_r1.x }, vec3f { r1_r2.y, r1_r3.y, p_r1.y });
-
-    if (std::abs(vecRes.z) < 1e-2)
-        // if (std::abs(vecRes.z) < 1)
-        return vec3f { -1, 1, 1 };
-
-    vecRes = vecRes / vecRes.z;
-    return (vec3f { 1.0f - (vecRes.x + vecRes.y), vecRes.x, vecRes.y });
-}
-
-vec3f toBarycentricCoord(vec2f* verts, vec2f point) {
-    vec3f r1_r2 = verts[1] - verts[0];
-    vec3f r1_r3 = verts[2] - verts[0];
-    vec3f p_r1 = verts[0] - point;
-
-    vec3f vecRes = crossProduct(vec3f { r1_r2.x, r1_r3.x, p_r1.x }, vec3f { r1_r2.y, r1_r3.y, p_r1.y });
-
-    if (std::abs(vecRes.z) < 1e-2)
-        // if (std::abs(vecRes.z) < 1)
-        return vec3f { -1, 1, 1 };
-
-    vecRes = vecRes / vecRes.z;
-    return (vec3f { 1.0f - (vecRes.x + vecRes.y), vecRes.x, vecRes.y });
-}
-
 Matrix vecToMat(const vec2f& vec) {
     std::cerr << "Ejecutado\n";
     Matrix mat { 3, 1 };
--- src/math.hpp
+++ src/math.hpp
@@ -43,22 +43,22 @@ vec2<T>::operator vec2<U>() {
 // Operaciones basicas vec2
 
 template <typename T>
-const vec2<T> operator+(const vec2<T>& v1, const vec2<T>& v2) {
+inline const vec2<T> operator+(const vec2<T>& v1, const vec2<T>& v2) {
     return vec2<T> { v1.x + v2.x, v1.y + v2.y };
 }
 
 template <typename T>
-const vec2<T> operator-(const vec2<T>& v1, const vec2<T>& v2) {
+inline const vec2<T> operator-(const vec2<T>& v1, const vec2<T>& v2) {
     return vec2<T> { v1.x - v2.x, v1.y - v2.y };
 }
 
 template <typename T>
-const vec2<T> operator*(const vec2<T>& v1, const T k) {
+inline const vec2<T> operator*(const vec2<T>& v1, const T k) {
     return vec2<T> { v1.x * k, v1.y * k };
 }
 
 template <typename T>
-const vec2<T> operator/(const vec2<T>& v1, const T k) {
+inline const vec2<T> operator/(const vec2<T>& v1, const T k) {
     return vec2<T> { v1.x / k, v1.y / k };
 }
 
@@ -91,25 +91,25 @@ vec3<T>::operator vec3<U>() {
     return vec3<U> { (U)((*this).x), (U)((*this).y), (U)((*this).z) };
 }
 
-// Operaciones basicas vec2
+// Operaciones basicas vec3
 
 template <typename T>
-const vec3<T> operator+(const vec3<T>& v1, const vec3<T>& v2) {
+inline const vec3<T> operator+(const vec3<T>& v1, const vec3<T>& v2) {
     return vec3<T> { v1.x + v2.x, v1.y + v2.y, v1.z + v2.z };
 }
 
 template <typename T>
-const vec3<T> operator-(const vec3<T>& v1, const vec3<T>& v2) {
+inline const vec3<T> operator-(const vec3<T>& v1, const vec3<T>& v2) {
     return vec3<T> { v1.x - v2.x, v1.y - v2.y, v1.z - v2.z };
 }
 
 template <typename T>
-const vec3<T> operator*(const vec3<T>& v1, const T k) {
+inline const vec3<T> operator*(const vec3<T>& v1, const T k) {
     return vec3<T> { v1.x * k, v1.y * k, v1.z * k };
 }
 
 template <typename T>
-const vec3<T> operator/(const vec3<T>& v1, const T k) {
+inline const vec3<T> operator/(const vec3<T>& v1, const T k) {
     return vec3<T> { v1.x / k, v1.y / k, v1.z / k };
 }
 
@@ -127,12 +127,12 @@ vec2<T> discard_Z(vec3<T> v) {
 // Productos entre vectores
 
 template <typename T>
-const T dotProduct(const vec3<T>& v1, const vec3<T>& v2) {
+inline const T dotProduct(const vec3<T>& v1, const vec3<T>& v2) {
     return T { v1.x * v2.x + v1.y * v2.y + v1.z * v2.z };
 }
 
 template <typename T>
-const vec3<T> crossProduct(const vec3<T>& v1, const vec3<T>& v2) {
+inline const vec3<T> crossProduct(const vec3<T>& v1, const vec3<T>& v2) {
     return vec3<T> { v1.y * v2.z - v2.y * v1.z, v2.x * v1.z - v1.x * v2.z, v1.x * v2.y - v2.x * v1.y };
 }
 
@@ -232,8 +232,38 @@ T dotProduct(const vec<size, T>& lhs, const vec<size, T>& rhs) {
 
 ////////////////////////////////////////////////////////////////
 // Geometric Baricentric Coords
-vec3f toBarycentricCoord(vec3f* verts, vec2f point);
-vec3f toBarycentricCoord(vec2f* verts, vec2f point);
+inline vec3f toBarycentricCoord(vec3f* verts, vec2f point) {
+    // Asumiendo r1, r2, r3 los vertices del triangulo, y p el punto
+    // Variables que representan vectores matematicos
+    vec3f r1_r2 = verts[1] - verts[0];
+    vec3f r1_r3 = verts[2] - verts[0];
+    vec3f p_r1 = verts[0] - (vec3f)point;
+
+    // vec3f x_values { r1_r2.x, r1_r3.x, p_r1.x };
+    // vec3f y_values { r1_r2.y, r1_r3.y, p_r1.y };
+    vec3f vecRes = crossProduct(vec3f { r1_r2.x, r1_r3.x, p_r1.x }, vec3f { r1_r2.y, r1_r3.y, p_r1.y });
+
+    if (std::abs(vecRes.z) < 1e-2)
+        // if (std::abs(vecRes.z) < 1)
+        return vec3f { -1, 1, 1 };
+
+    vecRes = vecRes / vecRes.z;
+    return (vec3f { 1.0f - (vecRes.x + vecRes.y), vecRes.x, vecRes.y });
+}
+inline vec3f toBarycentricCoord(vec2f* verts, vec2f point) {
+    vec3f r1_r2 = verts[1] - verts[0];
+    vec3f r1_r3 = verts[2] - verts[0];
+    vec3f p_r1 = verts[0] - point;
+
+    vec3f vecRes = crossProduct(vec3f { r1_r2.x, r1_r3.x, p_r1.x }, vec3f { r1_r2.y, r1_r3.y, p_r1.y });
+
+    if (std::abs(vecRes.z) < 1e-2)
+        // if (std::abs(vecRes.z) < 1)
+        return vec3f { -1, 1, 1 };
+
+    vecRes = vecRes / vecRes.z;
+    return (vec3f { 1.0f - (vecRes.x + vecRes.y), vecRes.x, vecRes.y });
+}
 
 ////////////////////////////////////////////////////////////////
 // some Matrix functions 
