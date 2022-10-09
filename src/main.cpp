0796d6b88ab0326457b1731a9544267b6c1e8090
Thgear27 <vegasfernando2003@gmail.com>
2022-10-09 13:38:25 -0500

    test

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,30 @@ constexpr int width  { 1000 };
 /**
  * TODO:
  * Añadir la componente z en la funcion doTransformation para determinara desde donde se hara la tranformacion
+ * Hacer que la proyección se añada al final y no modifique las coordenadas reales
  * Hacer la funcion viewport (que devuelva la matrix de trasformacion)
  * Hacer la funcion lookAt
  */
 
 int main() {
     TGAImage img { width, height, TGAImage::Format::RGB };
-    Model modelo(boogie_body, Model::Format::with_vt);
+    Model modelo(cubePortal, Model::Format::with_vt);
 
-    Renderer renderer(modelo, img, boogie_body_texture_img);
-    renderer.setLightDirection(vec3f {-1, 0, 1});
+    Renderer renderer(modelo, img, cubePortalTexture_img);
+    renderer.setLightDirection(vec3f { -1, -1, 1 });
     renderer.setViewport();
     
-    renderer.doTransformation(my_gl::zoom(0.6f), width / 2, height / 2);
-    renderer.doTransformation(my_gl::rotatey(1.2f), 0, 0);
-    renderer.doTransformation(my_gl::simpleProjection(800), width / 2, height / 2);
+    renderer.doTransformation(my_gl::zoom(0.4f), vec3f{ width / 2, height / 2, 0 });
+    renderer.doTransformation(my_gl::rotatey(M_PI/4), modelo.getModelCenter());
+    renderer.doTransformation(my_gl::rotatex(-M_PI/4), modelo.getModelCenter());
+    renderer.doTransformation(my_gl::simpleProjection(800), vec3f{ width / 2, height / 2, 0 });
 
     renderer.render();
     renderer.clearImg();
     renderer.render();
     
-    std::string file = "hola" + std::to_string(1) + ".tga";
+    std::string file = "hola" + std::to_string(2) + ".tga";
+    std::cout << modelo.getModelCenter() << '\n';
 
     renderer.generateImg(file);
 
