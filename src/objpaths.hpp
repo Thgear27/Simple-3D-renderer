13d36d4cdf979e69b95317e7bf0fe14883ab5ade
Thgear27 <vegasfernando2003@gmail.com>
2022-10-16 20:57:18 -0500

    obj_paths updated

--- src/objpaths.hpp
+++ src/objpaths.hpp
@@ -3,9 +3,11 @@
 
 const char* african_head             = "obj/african_head/african_head.obj";
 const char* african_head_texture_img = "obj/african_head/african_head_diffuse.tga";
+const char* african_head_nm          = "obj/african_head/african_head_nm.tga";
 
 const char* ae_inner                 = "obj/african_head/african_head_eye_inner.obj";
 const char* ae_inner_texture_img     = "obj/african_head/african_head_eye_inner_diffuse.tga";
+const char* ae_inner_nm              = "obj/african_head/african_head_eye_inner_nm.tga";
 
 const char* ae_outer                 = "obj/african_head/african_head_eye_outer.obj";
 const char* ae_outer_texture_img     = "obj/african_head/african_head_eye_outer_diffuse.tga";
@@ -18,8 +20,12 @@ const char* boogie_body_texture_img  = "obj/boggie/body_diffuse.tga";
 
 const char* diablo3pose              = "obj/diablo3_pose/diablo3_pose.obj";
 const char* diablo3pose_texture_img  = "obj/diablo3_pose/diablo3_pose_diffuse.tga";
+const char* diablo3pose_nm           = "obj/diablo3_pose/diablo3_pose_nm.tga";
 
 const char* cubePortal               = "obj/cubePortal/cube.obj";
 const char* cubePortalTexture_img    = "obj/cubePortal/metal_box_skin001.tga";
+
+const char* floor                    = "obj/floor.obj";
+const char* floor_nm_tangent         = "obj/floor_nm_tangent.tga";
 
 #endif // OBJPATHS_H
